5ee59c1e2dfe4b96c887855d32fe394d37e6112e
EC2 Default User <ec2-user@ip-172-31-8-53.ap-northeast-1.compute.internal>
2022-10-02 11:55:29 +0000

    [Add]マイグレーションファイル追加

--- config/routes.rb
+++ config/routes.rb
@@ -20,10 +20,10 @@ Rails.application.routes.draw do
   end
 # 顧客用
 # URL /customers/sign_in ...
-#devise_for :customers,skip: [:passwords], controllers: {
-  #registrations: "public/registrations",
-  #sessions: 'public/sessions'
-#}
+devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
 
 # 管理者用
 # URL /admin/sign_in ...

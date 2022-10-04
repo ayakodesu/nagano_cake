55d36f3073192ac15123278e638c013d9f2c6533
EC2 Default User <ec2-user@ip-172-31-8-53.ap-northeast-1.compute.internal>
2022-10-04 13:10:37 +0000

    [Add]admin index編集とedit編集

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,14 +1,27 @@
 class Admin::GenresController < ApplicationController
   def new
-    @admin = Admin.new
+    @genre = Genre.new
   end
 
   def index
-    @admins = Admin.all
+    @genre = Genre.new
+    @genres = Genre.all
   end
 
   def edit
-    @admin = Admin.find(params[:id])
+
   end
 
+  def create
+    @genre = Genre.new(genre_params)
+    @genre.save
+    redirect_to admin_genres_path(@genre.id)
+  end
+
+
 end
+
+private
+  def genre_params
+    params.require(:genre).permit(:name)
+  end

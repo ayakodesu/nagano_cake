bfd01a1ff60a5320d7bf32aabb5e424dc41a4acf
EC2 Default User <ec2-user@ip-172-31-8-53.ap-northeast-1.compute.internal>
2022-10-16 13:26:27 +0000

    [Add]顧客側ヘッダー

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -24,6 +24,10 @@ class Public::SessionsController < Devise::SessionsController
     public_path
   end
 
+  def after_sign_out_path_for(resource)
+    public_path
+  end
+
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])

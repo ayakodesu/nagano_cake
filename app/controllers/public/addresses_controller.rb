64143ac01263864b984f0e2367f482f51f4c2aeb
EC2 Default User <ec2-user@ip-172-31-8-53.ap-northeast-1.compute.internal>
2022-10-20 14:41:21 +0000

    [Add]顧客の配送先登録一覧

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,7 +1,10 @@
 class Public::AddressesController < ApplicationController
   def index
+    @customer = Customer.new
+    @customers = Customer.all
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 end

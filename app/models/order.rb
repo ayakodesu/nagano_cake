ec571866653e35b2a5d89203b23469ab56d9f2d9
EC2 Default User <ec2-user@ip-172-31-8-53.ap-northeast-1.compute.internal>
2022-10-22 09:05:15 +0000

    [Add]アソシエーション

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,4 +5,8 @@ class Order < ApplicationRecord
 
   enum status: { waiting_for_payment: 0, payment_confirmation: 1, production: 2, shipping_preparation: 3, sent: 4 }
 
+  belongs_to :customer
+  has_many :order_details, dependent: :destroy
+
+
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,4 +1,8 @@
 class OrderDetail < ApplicationRecord
 
   enum making_method: { cannot_be_manufactured: 0, waiting_for_production: 1, production: 2,　production_completed: 3 }
+
+  belongs_to :order
+  belongs_to :item
+
 end

580baa3068c2a70948fca811e1185d04ba31ece3
EC2 Default User <ec2-user@ip-172-31-8-53.ap-northeast-1.compute.internal>
2022-10-27 14:36:09 +0000

    [Add]注文情報入力

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,5 +1,6 @@
 class Public::OrdersController < ApplicationController
   def new
+    @order = Order.new
   end
 
   def complete
@@ -9,5 +10,10 @@ class Public::OrdersController < ApplicationController
   end
 
   def show
+  end
+
+   private
+  def order_params
+    params.require(:order).permit(:customer_id, :postal_code, :address, :name, :shipping_cost, :total_payment, :payment_method, :status)
   end
 end

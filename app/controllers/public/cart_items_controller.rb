35c339e5c2eee00f640558c37999ab9ec0892e89
EC2 Default User <ec2-user@ip-172-31-8-53.ap-northeast-1.compute.internal>
2022-10-29 07:48:17 +0000

    [Add]カート内数量変更

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -11,8 +11,8 @@ class Public::CartItemsController < ApplicationController
     @cart_item = current_customer.cart_items.new(cart_item_params)
     @cart_item.save
     redirect_to public_cart_items_path
-      end
   end
+
 
   def destroy
     @cart_item = CartItem.find(params[:id])
@@ -27,12 +27,17 @@ class Public::CartItemsController < ApplicationController
     redirect_to public_cart_items_path
   end
 
+  def update
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.update(cart_item_params)
+    redirect_to public_cart_items_path
+  end
+
 
 
 
 private
   def cart_item_params
     params.require(:cart_item).permit(:item_id, :customer_id, :amount)
-  end
   end
 end

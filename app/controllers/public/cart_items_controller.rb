968cc7f166b240b6fc4a5a88676595c657cccfa0
EC2 Default User <ec2-user@ip-172-31-8-53.ap-northeast-1.compute.internal>
2022-10-29 09:12:47 +0000

    [Add]カート内商品同じ商品追加

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -9,8 +9,18 @@ class Public::CartItemsController < ApplicationController
 
   def create
     @cart_item = current_customer.cart_items.new(cart_item_params)
-    @cart_item.save
-    redirect_to public_cart_items_path
+    if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
+        cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+          cart_item.amount += params[:cart_item][:amount].to_i
+            @cart_item.save
+              redirect_to public_cart_items_path
+
+    elsif @cart_item.save
+      @cart_items = current_customer.cart_items.all
+      redirect_to public_cart_items_path
+    else
+      redirect_to public_cart_items_path
+    end
   end
 
 

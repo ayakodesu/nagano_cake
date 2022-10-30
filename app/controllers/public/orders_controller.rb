da10e6a42eb13f00d8b95a29c379b4dc795ad7a7
EC2 Default User <ec2-user@ip-172-31-8-53.ap-northeast-1.compute.internal>
2022-10-30 12:37:03 +0000

    [Add]注文情報確認

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -2,7 +2,7 @@ class Public::OrdersController < ApplicationController
   def new
     @order = Order.new
     @customer = current_customer
-    @addresses = current_customer.addresses
+
   end
 
   def create
@@ -21,8 +21,22 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
+    @order = Order.new(order_params)
+    @select_address = params[:order][:select_address]
+    if @select_address == "0"
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = current_customer.last_name + current_customer.first_name
+    elsif @select_address == "1"
+      @address_id = params[:order][:address_id]
+      @address = Address.find(@address_id)
+      @order.postal_code = @address.postal_code
+      @order.address = @address.address
+      @order.name = @address.name
+    else
 
-    @cart_items = current_customer.cart_items
+    end
+
   end
 
    private
--- app/models/order.rb
+++ app/models/order.rb
@@ -9,4 +9,6 @@ class Order < ApplicationRecord
   has_many :order_details, dependent: :destroy
 
 
+
+
 end

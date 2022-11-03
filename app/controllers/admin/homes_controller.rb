5340a3fdad170425437b1a9d9a0bf21f7683ea3d
EC2 Default User <ec2-user@ip-172-31-8-53.ap-northeast-1.compute.internal>
2022-11-03 13:34:28 +0000

    [Add]管理者側　顧客注文履歴

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,8 +1,9 @@
 class Admin::HomesController < ApplicationController
 
   def top
-    @orders = Order.all
-    @orderDetails = OrderDetail.all
+    @orders = Order.all.page(params[:page])
+
     @items = Item.order('id DESC').limit(4)
   end
 end
+
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,6 +1,6 @@
 class Admin::OrdersController < ApplicationController
   def show
-
+    @orders = current_customer.orders
 
   end
 end

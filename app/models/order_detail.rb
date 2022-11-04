442f6ce12fd4211373541dfa8cedccf7272d3596
EC2 Default User <ec2-user@ip-172-31-8-53.ap-northeast-1.compute.internal>
2022-11-04 14:34:59 +0000

    [Add]顧客注文履歴詳細ページ作成

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,6 +1,6 @@
 class OrderDetail < ApplicationRecord
 
-  enum making_method: { cannot_be_manufactured: 0, waiting_for_production: 1, production: 2,　production_completed: 3 }
+  enum making_method: { cannot_be_manufactured: 0, waiting_for_production: 1, production: 2,  production_completed: 3 }
 
   belongs_to :order
   belongs_to :item

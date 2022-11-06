12e96dd527fc5eecaa9124f0ea00ded7823af73b
EC2 Default User <ec2-user@ip-172-31-8-53.ap-northeast-1.compute.internal>
2022-11-06 04:24:15 +0000

    [Add]商品詳細税込み金額表示

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -5,7 +5,7 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_account_update_params, only: [:update]
 
   def after_sign_up_path_for(resource)
-    public_path
+    public_customer_path
   end
 
   # GET /resource/sign_up
